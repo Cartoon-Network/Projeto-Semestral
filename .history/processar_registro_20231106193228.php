91768ad78d3a283fafeb9e1828a3d5bb607f27ce
Matheus904-17 <67837282+Matheus904-12@users.noreply.github.com>
2023-11-06 20:32:54 -0300

    vai

---
+++ .history/processar_registro_20231106193228.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost"; // Host do banco de dados
+$username = "seu_usuario"; // Nome de usuário do banco de dados
+$password = "sua_senha"; // Senha do banco de dados
+$dbname = "seu_banco_de_dados"; // Nome do banco de dados
+
+// Conexão com o banco de dados
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Verifica se a conexão foi estabelecida
+if ($conn->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+}
+?>
