bd59233ef5a9cf06a60a665745a1193be610fd11
Matheus904-17 <67837282+Matheus904-12@users.noreply.github.com>
2023-11-27 20:14:34 -0300

    vai

--- Contador/data.js
+++
@@ -1,9 +0,0 @@
-var data = new Date().getMonth()+2;
-console.log(data)
-
-var relogio = setInterval(function () {
-    let horas = new Date().getHours();
-    let minutos = new Date().getMinutes();
-    let segundos = new Date().getSeconds();
-    console.log(`${horas}:${minutos}:${segundos}`)
-}, 1000);
