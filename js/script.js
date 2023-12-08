a7f60433f44e90dd630a8f194137c132409f0919
Matheus904-17 <67837282+Matheus904-12@users.noreply.github.com>
2023-12-08 01:26:45 -0300

    vai

--- js/script.js
+++ js/script.js
@@ -187,12 +187,15 @@ function flipCard() {
 function validarEntrada() {
   const emailEntrada = document.getElementById('email').value;
   const senhaEntrada = document.getElementById('senha').value;
+  const botaoEntrar = document.getElementById('botaoEntrar');
 
   if (!emailEntrada || !senhaEntrada) {
-    alert('Por favor, preencha todos os campos antes de entrar.');
+      alert('Por favor, preencha todos os campos antes de entrar.');
+      botaoEntrar.disabled = true;
   } else {
-    // Continue com a lógica de login
-    // signIn(emailEntrada, senhaEntrada);
+      botaoEntrar.disabled = false;
+      // Continue com a lógica de login
+      // signIn(emailEntrada, senhaEntrada);
   }
 }
 
@@ -200,14 +203,18 @@ function validarCadastro() {
   const nomeCadastro = document.getElementById('nome').value;
   const emailCadastro = document.getElementById('cadastroEmail').value;
   const senhaCadastro = document.getElementById('cadastroSenha').value;
+  const botaoCadastrar = document.getElementById('botaoCadastrar');
 
   if (!nomeCadastro || !emailCadastro || !senhaCadastro) {
-    alert('Por favor, preencha todos os campos antes de cadastrar.');
+      alert('Por favor, preencha todos os campos antes de cadastrar.');
+      botaoCadastrar.disabled = true;
   } else {
-    // Continue com a lógica de cadastro
-    // cadastrar(nomeCadastro, emailCadastro, senhaCadastro);
+      botaoCadastrar.disabled = false;
+      // Continue com a lógica de cadastro
+      // cadastrar(nomeCadastro, emailCadastro, senhaCadastro);
   }
 }
+
 function entrar() {
   // Obter os valores dos campos de login
   var email = document.getElementById('loginEmail').value;
@@ -216,14 +223,14 @@ function entrar() {
   // Verificar se o usuário está cadastrado
   var usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
   var usuarioEncontrado = usuariosCadastrados.find(function (user) {
-    return user.email === email && user.senha === senha;
+      return user.email === email && user.senha === senha;
   });
 
   if (usuarioEncontrado) {
-    alert('Login bem-sucedido!');
-    // Lógica para redirecionar ou realizar outras ações após o login
+      alert('Login bem-sucedido!');
+      // Lógica para redirecionar ou realizar outras ações após o login
   } else {
-    alert('Usuário não encontrado. Verifique suas credenciais.');
+      alert('Usuário não encontrado. Verifique suas credenciais.');
   }
 }
 
@@ -234,34 +241,37 @@ function cadastrar() {
   var senha = document.getElementById('cadastroSenha').value;
   var fotoRegistrada = localStorage.getItem('fotoRegistrada');
 
-
   // Verificar se o email já está cadastrado
   var usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
   var emailExistente = usuariosCadastrados.some(function (user) {
-    return user.email === email;
+      return user.email === email;
   });
 
   if (emailExistente) {
-    alert('Este email já está cadastrado. Por favor, use outro.');
+      alert('Este email já está cadastrado. Por favor, use outro.');
+  } else if (!nome || !email || !senha) {
+      alert('Por favor, preencha todos os campos antes de cadastrar.');
   } else {
-    // Adicionar novo usuário ao armazenamento local
-    var novoUsuario = { nome: nome, email: email, senha: senha, foto: fotoRegistrada };
-    usuariosCadastrados.push(novoUsuario);
-    localStorage.setItem('usuarios', JSON.stringify(usuariosCadastrados));
+      // Adicionar novo usuário ao armazenamento local
+      var novoUsuario = { nome: nome, email: email, senha: senha, foto: fotoRegistrada };
+      usuariosCadastrados.push(novoUsuario);
+      localStorage.setItem('usuarios', JSON.stringify(usuariosCadastrados));
 
-    // Limpar as informações do formulário
-    document.getElementById('cadastroNome').value = '';
-    document.getElementById('cadastroEmail').value = '';
-    document.getElementById('cadastroSenha').value = '';
-    document.getElementById('fotoRegistrada').value = '';
+      // Limpar as informações do formulário
+      document.getElementById('cadastroNome').value = '';
+      document.getElementById('cadastroEmail').value = '';
+      document.getElementById('cadastroSenha').value = '';
+      document.getElementById('fotoRegistrada').value = '';
 
-    // Limpar as informações do usuário
-    limparInformacoes();
+      // Limpar as informações do usuário
+      limparInformacoes();
 
-    alert('Cadastro bem-sucedido! Você pode fazer login agora.');
-    // Lógica para redirecionar ou realizar outras ações após o cadastro
+      alert('Cadastro bem-sucedido! Você pode fazer login agora.');
+      // Lógica para redirecionar ou realizar outras ações após o cadastro
   }
 }
+
+
     function mostrarSubpagina(imagemSrc, titulo, descricao) {
         // Preencher os dados da subpágina
         document.getElementById('subpagina-imagem').innerHTML = '<img src="' + imagemSrc + '" alt="Imagem do Projeto">';
